cd6ec8489014fedeed98ab9520b871258d76c77f
IanKamau <theurikamau7@gmail.com>
2023-09-14 18:11:34 +0300

    completed choosebox component

--- src/ChooseBox.js
+++ src/ChooseBox.js
@@ -1,7 +1,11 @@
 function ChooseBox ({img, title, text}) {
     return (
         <div className="choose-box">
-
+            <i>{img}</i>
+            <div className="choose-box__text">
+                <h4>{title}</h4>
+                <p>{text}</p>
+            </div>
         </div>
     );
 }
